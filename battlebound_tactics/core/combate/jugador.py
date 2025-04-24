efe9304797be9c840c0db59c81145b7522f56602
Jonatan <jgarlun2112@g.educaand.es>
2025-04-24 18:00:01 +0200

    Avances del jueves por la mañana

---
+++ battlebound_tactics/core/combate/jugador.py
@@ -0,0 +1,13 @@
+def __init__(self, jugador):
+    self.jugador = jugador
+    self.salud_max = jugador.salud_maxima
+    self.salud = jugador.salud
+    self.energia = jugador.energia_espiritual
+    self.energia_max = jugador.energia_espiritual_maxima
+    self.ataque = jugador.ataque
+    self.defensa = jugador.defensa
+    self.velocidad = jugador.velocidad
+    self.arma = jugador.arma
+    self.accesorio = jugador.accesoiro
+    self.habilidad_pasiva = jugador.habilidad_pasiva
+
