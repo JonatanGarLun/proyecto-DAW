5a9eed73bc75ce9c2c7b7b866a125e73b75a2aaa
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-05-02 14:19:07 +0200

    cambios en el admin.py

--- battlebound_tactics/admin.py
+++ battlebound_tactics/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import (
     Jugador, Mochila, Objeto, ObjetoEnMochila,
     Arma, Accesorio, Pasiva,
-    HiddenPotential, HiddenPotentialNodeTemplate,
     Combate, Turno,
     Estado, EstadoActivo,
     Enemigo, Jefe
@@ -21,8 +20,6 @@ admin.site.register(Arma)
 admin.site.register(Accesorio)
 admin.site.register(Pasiva)
 
-admin.site.register(HiddenPotential)
-admin.site.register(HiddenPotentialNodeTemplate)
 
 admin.site.register(Combate)
 admin.site.register(Turno)
